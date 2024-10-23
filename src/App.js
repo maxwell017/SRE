a486b65a34ae39f59ab6c0464bd9e2dffd23f222
Mandhala <dileep-kumar-reddy.mandhala@capgemini.com>
2024-10-23 13:23:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+         <h3>welcome to the world of SRE </h3>
+         
+    </div>
+  );
+}
+
+export default App;
